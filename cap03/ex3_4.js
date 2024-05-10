d3fae910f3e0602bb7bd633db6f0b30135713a8b
isammt <bella.trigo14@gmail.com>
2024-05-10 20:21:04 -0300

    exemplos do cap3 de node.js

---
+++ cap03/ex3_4.js
@@ -0,0 +1,8 @@
+const prompt = require("prompt-sync")()
+const pesoKg = Number(prompt("Peso da Ração (Kg):"))
+const consumo = Number(prompt("Consumo Diário (gr):"))
+const pesoGr = pesoKg * 1000
+const duracao = Math.floor(pesoGr / consumo)
+const sobra = pesoGr % consumo
+console.log(`Duracção: ${duracao} dias`)
+console.log(`Sobra: ${sobra}gr`)
